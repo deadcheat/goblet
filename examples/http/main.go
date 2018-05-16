3fd0a802355fe6ad015338da12aa2d8fd61bbd7a
deadcheat <deadcheat@users.noreply.github.com>
2018-05-16 12:56:06 +0900

    added example

---
+++ examples/http/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/deadcheat/awsset/examples/http/assetsbin"
+)
+
+func main() {
+	http.Handle("/static/", http.FileServer(assetsbin.Assets.WithPrefix("/static/")))
+	log.Println("start server localhost:3000")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
