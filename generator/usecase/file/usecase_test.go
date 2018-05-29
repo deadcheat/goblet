0f7c0c3daedfb78c3898ebf8f568ba882c8858fa
deadcheat <deadcheat@users.noreply.github.com>
2018-05-29 22:07:31 +0900

    added test for addFile

--- generator/usecase/file/usecase_test.go
+++ generator/usecase/file/usecase_test.go
@@ -2,6 +2,7 @@ package file
 
 import (
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -11,7 +12,7 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
-func TestAddFile_ForSingleFiles(t *testing.T) {
+func TestAddFileForSingleFiles(t *testing.T) {
 	// Prepare dir and file
 	contentStr := "hello world!"
 	content := []byte(contentStr)
@@ -61,5 +62,67 @@ func TestAddFile_ForSingleFiles(t *testing.T) {
 	err = u.addFile(closedPath)
 	if err == nil {
 		t.Error("addFile should return some error")
+	}
+}
+
+func TestAddFileForDirectory(t *testing.T) {
+	contentStr := "hello world!"
+	content := []byte(contentStr)
+	d := gonch.New("", "tmpdir")
+	defer d.Close() // clean up
+
+	// Prepare mock
+	c := gomock.NewController(t)
+	defer c.Finish()
+	m := mock.NewMockRegexpRepository(c)
+
+	// successfull pattern
+	filenameSuccess1 := "/child/success1.txt"
+	path1 := filepath.Join(d.Dir(), filenameSuccess1)
+	d.AddFile("success", filenameSuccess1, content, 0666)
+	m.EXPECT().MatchAny(path1).Return(true)
+
+	filenameSuccess2 := "/child/success2.txt"
+	path2 := filepath.Join(d.Dir(), filenameSuccess2)
+	d.AddFile("success", filenameSuccess2, content, 0666)
+	m.EXPECT().MatchAny(path2).Return(true)
+
+	// permission denied dir
+	deniedDir := "/permissiondeny"
+	if err := d.AddDir("faildir", deniedDir, 0000); err != nil {
+		panic(err)
+	}
+
+	// permission denied file in permitted dir
+	permittedDir := "/permitteddir"
+	if err := d.AddDir("permitteddir", permittedDir, os.ModePerm); err != nil {
+		panic(err)
+	}
+	deniedFile := filepath.Join(permittedDir, "permitted_denied.txt")
+	deniedFilePath := filepath.Join(d.Dir(), deniedFile)
+	err := d.AddFile("permitted_denied", deniedFile, content, 0000)
+	m.EXPECT().MatchAny(deniedFilePath).Return(true)
+
+	// create usecase
+	iu := New(m)
+	u := iu.(*UseCase)
+
+	// success pattern
+	targetDir := filepath.Join(d.Dir(), "/child")
+	err = u.addFile(targetDir)
+	if err != nil {
+		t.Error("addFile should not return any errors", err)
+	}
+
+	// when directory is not permittedf
+	err = u.addFile(filepath.Join(d.Dir(), deniedDir))
+	if err == nil {
+		t.Error("addFile should return any error when dir is denied")
+	}
+
+	// when file in dir is permitted
+	err = u.addFile(filepath.Join(d.Dir(), permittedDir))
+	if err == nil {
+		t.Error("addFile should return any error when file in dir is denied")
 	}
 }
