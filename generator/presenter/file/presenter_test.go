7ccd47ff9c2f153c49203f40eb72c810d4a2dd44
deadcheat <deadcheat@users.noreply.github.com>
2018-06-10 01:09:11 +0900

    added first test for presenter

--- generator/presenter/file/presenter_test.go
+++ generator/presenter/file/presenter_test.go
@@ -1,7 +1,28 @@
 package file
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/deadcheat/goblet/generator/mock"
+	"github.com/golang/mock/gomock"
+
+	"github.com/urfave/cli"
+)
 
 func TestMount(t *testing.T) {
+	// Success Pattern
+	a := &cli.App{}
+
+	// Prepare mock
+	c := gomock.NewController(t)
+	defer c.Finish()
+
+	m := mock.NewMockUseCase(c)
+
+	p := New(m)
+
+	if err := p.Mount(a); err != nil {
+		t.Error("Mount should not return any errors", err)
+	}
 
 }
