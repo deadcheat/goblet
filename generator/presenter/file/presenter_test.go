5c0b6063316fb69dae5cab8b59b42d740f312e8d
deadcheat <deadcheat@users.noreply.github.com>
2018-06-12 20:04:15 +0900

    added loadfiles error test

--- generator/presenter/file/presenter_test.go
+++ generator/presenter/file/presenter_test.go
@@ -1,6 +1,7 @@
 package file
 
 import (
+	"errors"
 	"flag"
 	"testing"
 
@@ -47,5 +48,31 @@ func TestActionNoArgs(t *testing.T) {
 	ctx := cli.NewContext(a, fs, nil)
 	if err := p.action(ctx); err != ErrNoArguments {
 		t.Error("Mount should return ErrNoArguments", err)
+	}
+}
+
+var errTest = errors.New("error test")
+
+func TestActionFailLoadFiles(t *testing.T) {
+
+	// Prepare mock
+	c := gomock.NewController(t)
+	defer c.Finish()
+
+	m := mock.NewMockUseCase(c)
+	m.EXPECT().LoadFiles([]string{"config"}, nil).Return(nil, errTest)
+
+	p := New(m)
+	a := &cli.App{}
+	p.Mount(a)
+	set := flag.NewFlagSet("test", flag.ContinueOnError)
+	if err := set.Parse([]string{"config"}); err != nil {
+		panic(err)
+	}
+
+	a.Flags = values.FlagDefs
+	ctx := cli.NewContext(a, set, nil)
+	if err := p.action(ctx); err != errTest {
+		t.Error("Mount should return errTest", err)
 	}
 }
