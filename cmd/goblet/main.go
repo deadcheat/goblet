450e6aa18c685b327097273f36aff688fab85a76
deadcheat <deadcheat@users.noreply.github.com>
2019-01-26 18:05:03 +0900

    fixed version

--- cmd/goblet/main.go
+++ cmd/goblet/main.go
@@ -19,7 +19,7 @@ func main() {
 
 	app.Name = "goblet"
 	app.Usage = "make a binary contain some assets"
-	app.Version = "1.2.2"
+	app.Version = "1.3.0"
 
 	app.Flags = values.FlagDefs
 	// mount presenter
